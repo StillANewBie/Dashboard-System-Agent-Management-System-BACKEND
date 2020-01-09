891d59a90756daf6f5ded1074ae48fccce169a7f
bo <boyu0107@gmail.com>
2020-01-09 14:25:52 -0500

    able to upload to aws s3

--- pm/src/main/java/com/mercury/pm/controllers/UserAdminController.java
+++ pm/src/main/java/com/mercury/pm/controllers/UserAdminController.java
@@ -1,5 +1,7 @@
 package com.mercury.pm.controllers;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.mercury.pm.beans.Group;
 import com.mercury.pm.beans.Login;
 import com.mercury.pm.services.GroupService;
@@ -38,20 +48,34 @@ public class UserAdminController {
 	}
 	
 	@PostMapping("/upload-image")
-	public String imageUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes ){
-		if (file.isEmpty()) {
-			return "empty";
+	public String imageUpload(@RequestParam("data") MultipartFile file, RedirectAttributes redirectAttributes ) throws IOException{
+		System.out.println(System.getProperty("java.io.tmpdir"));
+		File f = new File(System.getProperty("java.io.tmpdir") + "/" + "tmp.jpg");
+		try {
+			file.transferTo(f);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println(file.getName());
-		System.out.println(file.getContentType());
-		System.out.println(file.getOriginalFilename());
-		System.out.println(file.getSize());
-
+		AWSCredentials credentials = new BasicAWSCredentials(
+				  "AKIA4ODCR2OKWYQYQUXG", 
+				  "BYfH+jcI8GB+vnazbMvK2o+gHM553Q1Hg6i5KVu4"
+				);
+		
+		AmazonS3 s3client = AmazonS3ClientBuilder
+				  .standard()
+				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
+				  .withRegion(Regions.US_EAST_1)
+				  .build();
+		
+		String bucketName = "mercury-pm-images";
+		
+		if(s3client.doesBucketExist(bucketName)) {
+		    s3client.putObject(new PutObjectRequest(bucketName, "images/tmp.jpg", f).withCannedAcl(CannedAccessControlList.PublicRead));
+		    f.delete();
+		}
 		return "success";
 	}
+
 }
